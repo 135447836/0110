59cbc8afc4359048d5b5c4652ffb9c5f17b21100
135447836 <50473823+135447836@users.noreply.github.com>
2019-05-10 12:46:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
-    return res.end("Welcome to Heroku https://github.com/onplus/shadowsocks-heroku");
+    return res.end("Welcome to 0110 https://github.com/onplus/shadowsocks-0110");
   });
 
   wss = new WebSocketServer({
